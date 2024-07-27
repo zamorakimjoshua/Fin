d88930f8df3548d6d768772a3a95e888bc832005
zamorakimjoshua <163400925+zamorakimjoshua@users.noreply.github.com>
2024-07-27 13:07:46 +0700

    Add files via upload

---
+++ AccountManagementServices/UserTransactionServices.cs
@@ -0,0 +1,63 @@
+﻿using ClassroomManagementData;
+using ClassroomManagementModels;
+
+namespace ClassroomManagementServices
+{
+    public class UserTransactionServices
+    {
+        UserValidationServices validationServices = new UserValidationServices();
+        UserData userData = new UserData();
+
+        public bool CreateUser(User user)
+        {
+            bool result = false;
+
+            if (validationServices.CheckIfUserExists(user.prof, user.roomNum, user.status))
+            {
+                userData.AddUser(user);
+            }
+
+            return result;
+        }
+
+        public bool CreateUser(string prof, string roomNum, string status)
+        {
+            User user = new User { prof = prof, roomNum = roomNum, status = status };
+
+            return CreateUser(user);
+        }
+
+        public bool UpdateUser(User user)
+        {
+            bool result = validationServices.CheckIfProfExists(user.prof);
+
+            if (validationServices.CheckIfProfExists(user.prof))
+            {
+                result = userData.UpdateUser(user) > 0;
+            }
+
+            return result;
+        }
+
+        public bool UpdateUser(string prof, string roomNum, string status)
+        {
+            User user = new User { prof = prof, roomNum = roomNum, status = status };
+
+            return UpdateUser(user);
+        }
+
+        public bool DeleteUser(User user)
+        {
+            bool result = false;
+
+            if (validationServices.CheckIfProfExists(user.prof))
+            {
+                result = userData.DeleteUser(user) > 0;
+            }
+
+            return result;
+        }
+    }
+}
+
+
---
+++ AccountManagementServices/UserValidationServices.cs
@@ -0,0 +1,21 @@
+﻿namespace ClassroomManagementServices
+{
+    public class UserValidationServices
+    {
+
+        UserGetServices getservices = new UserGetServices();
+
+        public bool CheckIfProfExists(string prof)
+        {
+            bool result = getservices.GetUsers(prof) != null;
+            return result;
+        }
+
+        public bool CheckIfUserExists(string prof, string roomNum, string status)
+        {
+            bool result = getservices.GetUsers(prof, roomNum, status) != null;
+            return result;
+        }
+
+    }
+}
